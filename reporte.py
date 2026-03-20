5df46ed38f5c7bc3c33a0307bca087f3ec2a2102
JaimieneAltozano <violybeatriz2638@gmail.com>
2026-03-19 19:01:18 -0500

    reporte

---
+++ reporte.py
@@ -0,0 +1,8 @@
+import registro_productos
+import daily_income 
+
+def Reporte_final(product):
+    print("reporte del dia" )
+    total_pedidos=len(product)
+    print(total_pedidos)
+    total_ingresos =
